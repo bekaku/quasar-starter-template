3991abec07d645352964562abe6a88c39a3b7c66
bekaku <33171470+bekaku@users.noreply.github.com>
2025-03-14 16:49:54 +0700

    .

--- quasar.config.ts
+++ quasar.config.ts
@@ -58,7 +58,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
