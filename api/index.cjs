1df3455237fc12d2884cabac60f407e2ca59c2b1
bekaku <33171470+bekaku@users.noreply.github.com>
2026-07-11 15:35:11 +0700

    update vercel entrypoint

---
+++ api/index.cjs
@@ -0,0 +1,7 @@
+// Import the SSR app that Quasar has already built.
+
+// eslint-disable-next-line antfu/no-import-dist
+import SSRApp from '../dist/ssr/index.js';
+
+// Export to Vercel for further processing.
+export default SSRApp;
--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Import the SSR app that Quasar has already built.
-const SSRApp = require('../dist/ssr/index.js');
-
-// Export to Vercel for further processing.
-module.exports = SSRApp.default;
